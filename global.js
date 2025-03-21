c5c2b618d401148a332be1252b3ba3166d082013
Xleepree <diegoed1313@gmail.com>
2025-03-20 23:01:30 -0400

    v3
    i am bean

--- global.js
+++ global.js
@@ -1,10 +1,15 @@
 
-// main variables
+// variables
 
 let pointsPerClick = 1; // default click power
-let upgradeCost = 100; // initial cost
+let steroidsPrice = 100; // initial cost
 let clickCount = 0; // cps meter idle
 let cps = 0 // clicks per second
+let capybaraExists = 0;
+let monkeySlaveryActive = false;
+let monkeyPower = 1;
+let monkeyCost = 500;
+
 
 // point handling
 
@@ -20,7 +25,9 @@ function savePoints() {
     // stuffs to save
     localStorage.setItem("points", points);
     localStorage.setItem("pointsPerClick", pointsPerClick);
-    localStorage.setItem("upgradeCost", upgradeCost);
+    localStorage.setItem("steroidsPrice", steroidsPrice);
+    localStorage.setItem("capybara", capybaraExists);
+    localStorage.setItem("slave", monkeySlaveryActive);
 }
 
 function loadPoints() {
@@ -28,9 +35,16 @@ function loadPoints() {
     document.getElementById("points").innerText = savedPoints;
     
     pointsPerClick = Number(localStorage.getItem("pointsPerClick"));
-    upgradeCost = Number(localStorage.getItem("upgradeCost"));
+    steroidsPrice = Number(localStorage.getItem("steroidsPrice"));
+    capybaraExists = localStorage.getItem("capybara");
+    monkeySlaveryActive = localStorage.getItem("slave") === "true";
+    if (capybaraExists == 1) {
+        revealCapybara();
+    } else {
+        hideCapybara();
+    }
 
-    updateUpgradeButton();
+    updateSteroidsPrice();
     updateClickPowerDisplay();
 }
 
@@ -39,37 +53,81 @@ function resetPoints() {
     if (confirmReset == true) {
         document.getElementById("points").innerText = 0;
         pointsPerClick = 1;
-        upgradeCost = 100;
+        steroidsPrice = 100;
+        capybaraExists = 0;
+        monkeySlaveryActive = false;
+        clearInterval(monkeyInterval);
+        monkeyInterval = null;
 
-        updateUpgradeButton();
+        updateSteroidsPrice();
         updateClickPowerDisplay();
+        hideCapybara();
     }
 }
 
 // upgrade handling
 
-function upgradeClickPower() {
+function injectSteroids() {
     let points = Number(document.getElementById("points").innerText);
 
-    if (points >= upgradeCost) {
-        document.getElementById("points").innerText = points - upgradeCost // deduction
+    if (points >= steroidsPrice) {
+        document.getElementById("points").innerText = points - steroidsPrice // deduction
         pointsPerClick *= 2;
-        upgradeCost *= 2;
+        steroidsPrice *= 2;
 
-        updateUpgradeButton();
+        updateSteroidsPrice();
         updateClickPowerDisplay();
     } else {
         alert("not enough points.")
     }
 }
 
-function updateUpgradeButton() {
-    document.getElementById("upgradeButton").innerText = `x2 click power (${upgradeCost})`;
+function updateSteroidsPrice() {
+    document.getElementById("steroidsPrice").innerText = `${steroidsPrice} points`;
 }
 
 function updateClickPowerDisplay() {
     document.getElementById("clickPowerDisplay").innerText = `click power: ${pointsPerClick} points`;
 }
+
+function revealCapybara() {
+    capybaraExists = 1;
+    document.getElementById("capybara").style.opacity = "0.5";
+    document.getElementById("capybaraFren").style.backgroundColor = "lightgray";
+    document.getElementById("capybaraFren").onclick = "purchased.";
+}
+
+function hideCapybara() {
+    capybaraExists = 0;
+    document.getElementById("capybara").style.opacity = "0";
+    document.getElementById("capybaraFren").style.backgroundColor = "white";
+    document.getElementById("capybaraFren").onclick = revealCapybara;
+}
+
+    // slave
+
+    function enslaveMonkey() {
+        let points = Number(document.getElementById("points").innerText);
+
+        if (points >= monkeyCost) {
+            document.getElementById("points").innerText = points - monkeyCost;
+            monkeySlaveryActive = true;
+            startMonkeyWork();
+        } else {
+            alert("not enough points.")
+        }
+    }
+
+    function startMonkeyWork() {
+        if (!monkeySlaveryActive) return;
+        document.getElementById("monkeySlavery").style.backgroundColor = "lightgray";
+        document.getElementById("monkeySlavery").onclick = "purchased.";
+
+        monkeyInterval = setInterval(() => {
+            let points = Number(document.getElementById("points").innerText);
+            document.getElementById("points").innerText = points + monkeyPower;
+        }, 2000)
+    }
 
 // cps meter handling
 
